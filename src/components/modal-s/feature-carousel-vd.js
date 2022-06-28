8eca85534f59305afeda8a591bd91c333a303b73
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-28 15:45:12 +0630

    fix dots bug

--- src/components/modal-s/feature-carousel-vd.js
+++ src/components/modal-s/feature-carousel-vd.js
@@ -15,7 +15,7 @@ const CarouselVideo = () => {
     autoplaySpeed: 12000,
   };
   return (
-    <div className="bg-black py-16">
+    <section className="bg-black py-16">
       <Slider {...settings} className=" flex items-center">
         <div className="px-2 md:px-10">
           <video
@@ -66,38 +66,14 @@ const CarouselVideo = () => {
             </p>
           </div>
         </div>
-        <div className="px-2 md:px-10">
+
+        <div className="px-2 md:px-10 ">
           <video
             autoPlay
             playsInline
             loop
             muted
-            className="rounded-xl md:rounded-3xl"
-          >
-            <source
-              src="/videos/Model-S-Interior-Carousel-2.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </div>
-        <div className="px-4 py-4 mt-10 shadow-md text-sm bg-black md:px-10 lg:px-20 xl:px-48">
-          <div className="lg:px-40">
-            <h3 className="text-white bold mb-6">Yoke Steering</h3>
-            <p className="text-white">
-              A bold new approach gives you a true connection to Model S,
-              offering better steering feel and unobstructed views of your dash
-              and the road ahead. Tap the brake and Model S automatically
-              selects the correct direction to start your trip.
-            </p>
-          </div>
-        </div>
-        <div className="px-2 md:px-10">
-          <video
-            autoPlay
-            playsInline
-            loop
-            muted
-            className="rounded-xl md:rounded-3xl"
+            className="rounded-xl md:rounded-3xl "
           >
             <source
               src="/videos/Model-S-Interior-Carousel-3.mp4"
@@ -125,24 +101,22 @@ const CarouselVideo = () => {
             className="rounded-xl md:rounded-3xl"
           >
             <source
-              src="/videos/Model-S-Interior-Carousel-3.mp4"
+              src="/videos/Model-S-Interior-Carousel-4.mp4"
               type="video/mp4"
             />
           </video>
         </div>
         <div className="px-4 py-4 mt-10 shadow-md text-sm bg-black md:px-10 lg:px-20 xl:px-48">
           <div className="lg:px-40">
-            <h3 className=" text-white bold mb-6"> Perfect Environment</h3>
+            <h3 className=" text-white bold mb-6"> Redesigned Second Row</h3>
             <p className="text-white">
-              {" "}
-              Air vents are hidden throughout the cabin, while tri-zone
-              temperature controls, ventilated seats and HEPA filtration deliver
-              the perfect environment.
+              Seating for three adults, with extra legroom, headroom and a
+              stowable armrest with integrated storage and wireless charging.
             </p>
           </div>
         </div>
       </Slider>
-    </div>
+    </section>
   );
 };
 
